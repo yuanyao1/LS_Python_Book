928f52344d1e0cf9702fed4640cd8f5ff3280f47
Yuan Yao <yyao.work@gmail.com>
2024-10-25 10:53:46 -0700

    first exercise in data types

---
+++ Data Types/ex_1.py
@@ -0,0 +1,10 @@
+print(type('True'))     #string
+print(type(False))      #boolean
+print(type((1, 2, 3)))  #tuple
+print(type(1.5))        #float
+print(type([1, 2, 3]))  #list
+print(type(2))          #int
+print(type(range(5)))   #range 0, 1, 2, 3, 4
+print(type({1, 2, 3}))  #set
+print(type(None))       #none
+print(type({'foo': 'bar'})) #dictionary
